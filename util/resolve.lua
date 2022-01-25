2365bc98a4b56f3c59271ba3d981d73dd06d7397
Ross Tyler <rossetyler@gmail.com>
2022-01-24 16:56:21 -0800

    string.pack, string.unpack network messages

--- util/resolve.lua
+++ util/resolve.lua
@@ -1,74 +1,73 @@
 local cosock = require "cosock"
 
 -- mDNS
-local MULTIADDR = "224.0.0.251"
+local ADDRESS = "224.0.0.251"
 local PORT = 5353
 
 -- resolve host name using mDNS. resolution is called with each address
 return function(name, resolution)
     local labels = {}
     for label in string.gmatch(name, "[%a%d-]+") do
-        table.insert(labels, string.char(#label))   -- length
-        table.insert(labels, label)                 -- label
+        table.insert(labels, string.pack("> s1", label))
     end
-    table.insert(labels, string.char(0))            -- end
     labels = table.concat(labels)
 
+    local transaction_id    = 0
+    local type              = 1 -- Type: A (Host Address0)
+    local class             = 1 -- Class: IN
     local question = table.concat({
-        string.char(
-            0, 0,       -- Transaction ID
-            0, 0,       -- Flags
-            0, 1,       -- Questions
-            0, 0,       -- Answer RRs
-            0, 0,       -- Authority RRs
-            0, 0        -- Additional RRs
+        string.pack("> I2 I2 I2 I2 I2 I2 z I2 I2",
+            transaction_id,
+            0,              -- Flags
+            1,              -- Questions
+            0,              -- Answer RRs
+            0,              -- Authority RRs
+            0,              -- Additional RRs
+            labels,
+            type,
+            class
         ),
-        labels,
-        string.char(
-            0, 1,       -- Type: A (Host Address)
-            0, 1        -- Class: IN
-        )
     })
 
-    local answer_prefix = table.concat({
-        string.char(
-            0, 0,       -- Transaction ID
-            0x84, 0,    -- Flags: Standard query response, No error
-            0, 0,       -- Questions
-            0, 1,       -- Answer RRs
-            0, 0,       -- Authority RRs
-            0, 0        -- Additional RRs (ignore)
-        ),
-        labels,
-        string.char(
-            0, 1,       -- Type: A (Host Address)
-            0, 1,       -- Class: IN, Cache flush: True (ignore)
-            0, 0, 0, 0, -- Time to live (ignore)
-            0, 4        -- Data length: 4
-        )
-        -- a, b, c, d   -- IPv4 address octets
-    })
+    local answer_len = 12 + (labels:len() + 1) + 14
 
     local resolver = cosock.socket.udp()
 
     -- ask question, expect unicast response to our ephemeral port
-    resolver:sendto(question, MULTIADDR, PORT)
+    resolver:sendto(question, ADDRESS, PORT)
 
     -- after a short wait, receive all answers and dispatch any resolution
     cosock.socket.sleep(0.1)
     while cosock.socket.select({resolver}, {}, 0.001) do
         local answer = resolver:receive(2048)
-        if #answer_prefix + 4 <= answer:len() then
-            local match = {answer:byte(1, #answer_prefix)}
-            match[11] = 0                   -- ignore Additional RRs
-            match[12] = 0                   -- ignore Additional RRs
-            match[#answer_prefix - 7] = 0   -- ignore Cache flush
-            match[#answer_prefix - 5] = 0   -- ignore TTL
-            match[#answer_prefix - 4] = 0   -- ignore TTL
-            match[#answer_prefix - 3] = 0   -- ignore TTL
-            match[#answer_prefix - 2] = 0   -- ignore TTL
-            if answer_prefix == string.char(table.unpack(match)) then
-                local address = table.concat({answer:byte(#answer_prefix + 1, #answer_prefix + 4)}, ".")
+
+        if answer_len <= answer:len() then
+            local
+                _transaction_id,
+                flags,
+                questions,
+                answers,
+                authorities,
+                additional,
+                _labels,
+                _type,
+                _class,
+                ttl,
+                length,
+                a, b, c, d
+            = string.unpack("> I2 I2 I2 I2 I2 I2 z I2 I2 I4 I2 BBBB", answer)
+            if true
+                and transaction_id  == _transaction_id
+                and 0x8400          == flags
+                and 0               == questions
+                and 1               == answers
+                and 0               == authorities
+                and labels          == _labels
+                and type            == _type
+                and class           == _class & ~0x8000 -- ignore Cache flush: True
+                and 4               == length
+            then
+                local address = table.concat({a, b, c, d}, ".")
                 resolution(address)
             end
         end
@@ -76,3 +75,4 @@ return function(name, resolution)
 
     resolver:close()
 end
+
