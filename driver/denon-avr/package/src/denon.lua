ab932b9553ea52ac7b846ef4b70fe16cef170b51
Ross Tyler <rossetyler@gmail.com>
2022-03-09 10:20:23 -0800

    denon.AVR, (re)constructable only with uuid and upnp

--- driver/denon-avr/package/src/denon.lua
+++ driver/denon-avr/package/src/denon.lua
@@ -6,70 +6,129 @@ local xml       = require "xml"
 local classify  = require "classify"
 local UPnP      = require "upnp"
 
-local M = {
-    Discover = classify.single({
+local RENDERING_CONTROL
+    = table.concat({UPnP.USN.UPNP_ORG, UPnP.USN.SERVICE_ID, "RenderingControl"}, ":")
 
-        -- default arguments
-        DISCOVER_BACKOFF_CAP    = 8,
+local M
 
-        _init = function(_, self, upnp)
+local Break = classify.error({})
+
+M = {
+    ST = UPnP.USN{[UPnP.USN.URN] = table.concat({UPnP.USN.SCHEMAS_UPNP_ORG, UPnP.USN.DEVICE, "MediaRenderer", 1}, ":")},
+
+    AVR = classify.single({
+        _init = function(_, self, uuid, upnp)
+            log.debug("AVR", uuid)
             self.upnp = upnp
-            self.media_renderer = UPnP.USN{[UPnP.USN.URN] = table.concat({UPnP.USN.SCHEMAS_UPNP_ORG, UPnP.USN.DEVICE, "MediaRenderer", 1}, ":")}
-            self.rendering_control = table.concat({UPnP.USN.UPNP_ORG, UPnP.USN.SERVICE_ID, "RenderingControl"}, ":")
+            self.uuid = uuid
+
+            -- self.eventing capture is garbage collected with self
             self.eventing = function(_, encoded)
                 pcall(function()
                     local event = xml.decode(encoded).root.Event.InstanceID
                     for _, name in ipairs{"Mute", "Volume"} do
                         local value = event[name]
                         if value then
-                            log.debug("Denon", name, value._attr.channel, value._attr.val)
+                            log.debug("AVR", name, value._attr.channel, value._attr.val)
                         end
                     end
                 end)
             end
-            self.discovery = function(address, port, header, description)
-                local device = description.root.device
-                if "Denon" == device.manufacturer then
-                    log.debug(device.friendlyName, address, port, header.location, tostring(header.usn))
+
+            -- search until location and device for uuid is found
+            local st = UPnP.USN{[UPnP.USN.UUID] = uuid}
+            cosock.spawn(function()
+                local function found(address, port, header, device)
+                    local found_uuid = header.usn.uuid
+                    if found_uuid ~= self.uuid then
+                        log.error("found", found_uuid, "not", self.uuid)
+                        return
+                    end
+                    log.debug("found", uuid, address, port, header.location, device.friendlyName)
+                    self.location = header.location
+                    self.device = device
                     for _, service in ipairs(device.serviceList.service) do
                         local urn = UPnP.USN(service.serviceId).urn
-                        if urn == self.rendering_control then
-                            upnp:eventing_subscribe(header.location, service.eventSubURL, header.usn.uuid, urn, nil, self.eventing)
+                        if urn == RENDERING_CONTROL then
+                            upnp:eventing_subscribe(
+                                header.location, service.eventSubURL, header.usn.uuid, urn, nil, self.eventing)
+                            -- unsubscribe is implicit on garbage collection of self
                             break
+                        end
+                        Break()
+                    end
+                end
+
+                local discover = M.Discover(upnp, st, found)
+                while true do
+                    local _, break_error = pcall(function()
+                        discover:search()
+                        cosock.socket.sleep(60)
+                    end)
+                    if break_error then
+                        local class_break_error = classify.class(break_error)
+                        if Break ~= class_break_error then
+                            error(break_error)
                         end
                     end
                 end
+            end, "find" .. tostring(st))
+        end,
+    }),
+
+    Discover = classify.single({
+        _init = function(_, self, upnp, st, found)
+            self.upnp = upnp
+            self.st = st
+            self.found = found
+
+            -- self.discovery capture is garbage collected with self
+            self.discovery = function(address, port, header, description)
+                local device = description.root.device
+                if "Denon" == device.manufacturer then
+                    self.found(address, port, header, device)
+                end
             end
-            upnp:discovery_subscribe(self.media_renderer, self.discovery)
+
+            upnp:discovery_subscribe(st, self.discovery)
+            -- unsubscribe is implicit on garbage collection of self
         end,
 
-        poll = function(self, backoff_cap, read_timeout)
-            backoff_cap = backoff_cap or self.DISCOVER_BACKOFF_CAP
-            if self._discover_sender then
-                self._discover_sender:send(0)
-            else
-                local sender, receiver = cosock.channel.new()
-                self._discover_sender = sender
-                cosock.spawn(function()
-                    local backoff = 0
-                    while true do
-                        self.upnp:discovery_search_multicast(self.media_renderer)
-                        -- rediscover after capped exponential backoff
-                        -- or our receiver hears from our sender.
-                        while cosock.socket.select({receiver}, {}, 8 << math.min(backoff, backoff_cap)) do
-                            log.debug("rediscover")
-                            backoff = receiver:receive()
-                        end
-                        backoff = backoff + 1
-                    end
-                end, "denon.Inventory.discover")
-            end
+        search = function(self)
+            self.upnp:discovery_search_multicast(self.st)
         end,
-    })
+    }),
 }
 
-local discover = M.Discover(UPnP("192.168.1.20"))
+local upnp = UPnP("192.168.1.20")
 
-discover:poll(1)
+local avr_set = {}
+
+local st = M.ST
+cosock.spawn(function()
+    local function found(address, port, header, device)
+        local uuid = header.usn.uuid
+        log.debug("found", uuid, address, port, header.location, device.friendlyName)
+        local avr = avr_set[uuid]
+        if not avr then
+            avr = M.AVR(uuid, upnp)
+            avr_set[uuid] = avr
+        end
+    end
+
+    local discover = M.Discover(upnp, st, found)
+    for _ = 1, 8 do
+        local _, break_error = pcall(function()
+            discover:search()
+            cosock.socket.sleep(8)
+        end)
+        if break_error then
+            local class_break_error = classify.class(break_error)
+            if Break ~= class_break_error then
+                error(break_error)
+            end
+        end
+    end
+end, "find" .. tostring(st))
 
 cosock.run()
